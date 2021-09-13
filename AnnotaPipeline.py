d4c5bb944c0d073e115f8c6c5788d429f9d5cffb
Vilmar <vilmarbf98@gmail.com>
2021-09-13 15:28:06 -0300

    bugfix

--- AnnotaPipeline.py
+++ AnnotaPipeline.py
@@ -997,8 +997,8 @@ else:
     else:
         first_last_param = str()
 
-
     mass_path = f"{str(comet.get('mass_files')).rstrip('/')}/"
+
     commet_command = f"{comet.get('comet_bash')} -P{comet.get('params')} " \
                 f"-D{annota_pwd / f'AnnotaPipeline_{AnnotaBasename}_proteins.fasta'} " \
                      f"{first_last_param} {mass_path}*"
@@ -1010,12 +1010,13 @@ else:
     
     logger.info("Parsing COMET output")
 
+    # Get all output files from mass_path >> default output path
     files = pathlib.Path(mass_path).glob('*.txt')
 
     for comet_output_file in files:
         logger.info(f"Parsing {comet_output_file}")
         parser_comet_comand = f"{python_exe} {str(pipeline_pwd / 'comet_parser.py')}" \
-                            f"-p {comet_output_file} -b {AnnotaBasename} "
+                            f" -p {comet_output_file} -b {AnnotaBasename}"
 
         if len(comet.get("charge")) != 0:
             parser_comet_comand += f" -ch {comet.get('charge')}"
@@ -1024,7 +1025,7 @@ else:
         except Exception as warn:
             logger.warning(f"Fail trying to parser {comet_output_file}")
             logger.debug(f"code error {warn}")
-            
+
     logger.info("COMET parsing is finished")
 
 # Return to AnnotaPipeline basedir
