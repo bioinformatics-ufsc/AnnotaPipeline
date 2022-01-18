9b75a4c77e0218f76b097c211d8e097d489898d7
pharaohs-son <60492463+pharaohs-son@users.noreply.github.com>
2022-01-18 19:03:40 -0300

    update setup com classifiers

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-# setar as versoes sertinho (>=x ou ==x)
 requirements = [
                 'biopython>=1.73',
                 'pandas>=0.24.1',
@@ -47,51 +46,23 @@ setup(
                 'Scripts/percolator_parser.py',
                 'Scripts/summary_parser.py'
              ],
-    license = "BSD",            #alterar pra licensa
+    license = "Apache", 
     include_package_data=True,
-    python_requires=">=3.6.9", # versao minima pra ter pathlib
+    python_requires=">=3.6.9",
     url='https://github.com/GuiMaia/AnnotaPipeline/tree/v1.0.git',
     download_url='https://github.com/GuiMaia/AnnotaPipeline/archive/refs/heads/v1.0.zip',
-
 # USA esse cara pra fz o classifier
 # https://pypi.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",    #alterar pra licensa
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: Portuguese (Brazilian)"
+        "Operating System :: Unix",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
     ],
 )
-
-# Pangolin exemple
-# from setuptools import setup, find_packages
-# import glob
-# import os
-# import pkg_resources
-
-# from pangolin import __version__, _program
-
-# setup(name='pangolin',
-#       version=__version__,
-#       packages=find_packages(),
-#       scripts=['pangolin/scripts/pangolearn.smk'
-#                 ],
-#       package_data={"pangolin":["data/*"]},
-#       install_requires=[
-#             "biopython>=1.70",
-#             'pandas>=1.0.1',
-#             "wheel>=0.34",
-#             'joblib>=0.11',
-#             'scikit-learn>=0.23.1',
-#             "PuLP>=2"
-#         ],
-#       description='phylogenetic assignment of named global outbreak lineages',
-#       url='https://github.com/cov-lineages/pangolin',
-#       author='Aine OToole & Emily Scher',
-#       author_email='aine.otoole@ed.ac.uk',
-#       entry_points="""
-#       [console_scripts]
-#       {program} = pangolin.command:main
-#       """.format(program = _program),
-#       include_package_data=True,
-#       keywords=[],
-#       zip_safe=False)
